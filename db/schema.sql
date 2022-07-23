642ed227625ce03c43a945eded631eb375da89d1
jdog <thuz3rfr13ndly@gmail.com>
2022-07-22 18:13:18 -0600

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,29 @@
 DELETE DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 
+USE DATABASE employee_tracker; 
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (department_name)
+);
+
+CREATE TABLE current_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    current_role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (current_role_id) REFERENCES current_role(id)
+);
