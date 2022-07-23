36adab9dc50bd4570d0f0a41fbec8dd6e9c19ecc
jdog <thuz3rfr13ndly@gmail.com>
2022-07-22 18:17:17 -0600

    connect db using dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: "localhost",
   // Your MySQL username,
-  user: "root",
-  // Your MySQL password
-  password: "Godisgood6111993", 
-  database: "employee_tracker",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 
